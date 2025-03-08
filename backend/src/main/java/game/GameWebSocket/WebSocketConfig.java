298385fcfc990f4be51a1a9750825dcf7fe2c798
thomastanjun <thomastanjun@gmail.com>
2025-03-08 10:07:48 -0800

    Fix web config

--- backend/src/main/java/game/GameWebSocket/WebSocketConfig.java
+++ backend/src/main/java/game/GameWebSocket/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/game")
-                .setAllowedOrigins("https://thomastanjun.github.io/24PointsGameOnline")
+                .setAllowedOrigins("https://thomastanjun.github.io")
                 .withSockJS();
     }
 
